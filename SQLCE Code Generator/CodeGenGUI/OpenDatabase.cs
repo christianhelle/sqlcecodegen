2674bfc9d82a9d62be2b71caeef1e0710e7a2adb
christianhelle <christianhelle@localhost>
2011-03-14 22:09:04 +0000

    GUI App: Added Open Database dialog to the project

---
+++ SQLCE Code Generator/CodeGenGUI/OpenDatabase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CodeGenGUI
+{
+    public partial class OpenDatabase : Form
+    {
+        public OpenDatabase()
+        {
+            InitializeComponent();
+        }
+
+        public string ConnectionString { get; private set; }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbDatabase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
