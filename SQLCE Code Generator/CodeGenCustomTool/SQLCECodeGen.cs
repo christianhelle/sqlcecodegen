3ca940dfebbb93e5a10873b870a08ecebed0cf61
christianhelle <christianhelle@localhost>
2011-05-04 14:31:57 +0000

    Resolved smart device project custom tool issue

--- SQLCE Code Generator/CodeGenCustomTool/SQLCECodeGen.cs
+++ SQLCE Code Generator/CodeGenCustomTool/SQLCECodeGen.cs
@@ -1,27 +1,66 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Forms;
 using ChristianHelle.DatabaseTools.SqlCe.CodeGenCore;
 using Microsoft.CustomTool;
-using System.Windows.Forms;
-using System.Diagnostics;
+using Microsoft.SqlServer.MessageBox;
 
 namespace ChristianHelle.DatabaseTools.SqlCe.CodeGenCustomTool
 {
     [Guid("64264FF6-2DD0-489a-A8C2-8FD7855FE3BF")]
     [ComVisible(true)]
-    public class SQLCECodeGenerator : BaseCodeGeneratorWithSite
+    public class SQLCECodeGenerator : IVsSingleFileGenerator
     {
-        protected override byte[] GenerateCode(string inputFileName, string inputFileContent)
+        #region IVsSingleFileGenerator Members
+
+        public string GetDefaultExtension()
         {
-            return GenerateCode(FileNameSpace, inputFileName, CodeProvider.FileExtension);
+            return ".cs";
         }
 
-        public static byte[] GenerateCode(string FileNameSpace, string inputFileName, string fileExtension)
+        public void Generate(string wszInputFilePath,
+                             string bstrInputFileContents,
+                             string wszDefaultNamespace,
+                             out IntPtr rgbOutputFileContents,
+                             out int pcbOutput,
+                             IVsGeneratorProgress pGenerateProgress)
+        {
+            try
+            {
+                var data = CodeGeneratorCustomTool.GenerateCode(wszDefaultNamespace, wszInputFilePath, "CSharp");
+                if (data == null)
+                {
+                    rgbOutputFileContents = IntPtr.Zero;
+                    pcbOutput = 0;
+                }
+                else
+                {
+                    rgbOutputFileContents = Marshal.AllocCoTaskMem(data.Length);
+                    Marshal.Copy(data, 0, rgbOutputFileContents, data.Length);
+                    pcbOutput = data.Length;
+                }
+            }
+            catch (Exception e)
+            {
+                var applicationException = new ApplicationException("Unable to generate code", e);
+                var messageBox = new ExceptionMessageBox(applicationException);
+                messageBox.Show(null);
+                throw;
+            }
+        }
+
+        #endregion
+    }
+
+    public static class CodeGeneratorCustomTool
+    {
+        public static byte[] GenerateCode(string fileNameSpace, string inputFileName, string fileExtension)
         {
             var fi = new FileInfo(inputFileName);
-            var generatedNamespace = FileNameSpace + "." + fi.Name.Replace(fi.Extension, string.Empty);
+            var generatedNamespace = fileNameSpace + "." + fi.Name.Replace(fi.Extension, string.Empty);
             var connectionString = "Data Source=" + inputFileName;
             var database = new SqlCeDatabase(generatedNamespace, connectionString);
             var factory = new CodeGeneratorFactory(database);
