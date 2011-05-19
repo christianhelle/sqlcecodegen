0f7dd675a9cabc384927da2b1a80add4ab70c68e
christianhelle <christianhelle@localhost>
2011-05-19 14:52:43 +0000

    Created custom tool for generating unit test code using mstest or nunit

---
+++ SQLCE Code Generator/CodeGenWPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GalaSoft.MvvmLight;
+using CodeGenWPF.Views;
+
+namespace CodeGenWPF.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+        }
+
+        public void ShowAboutBox()
+        {
+            var about = new AboutBox(null);
+            about.ShowDialog();
+        }
+    }
+}
