a52d6cbe565f06499db5374b61a95db73761e09a
Christian Helle <christian.helle@outlook.com>
2013-05-21 19:52:55 +0200

    Refatored CodeGenerationOptions into 2 seperate class files

---
+++ SQLCE Code Generator/CodeGenCore/EntityGeneratorOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace ChristianHelle.DatabaseTools.SqlCe.CodeGenCore
+{
+    public class EntityGeneratorOptions
+    {
+        public EntityGeneratorOptions()
+        {
+            ThrowExceptions = true;
+            AutoPropertiesOnly = false;
+            DebuggerOutput = false;
+        }
+
+        public bool ThrowExceptions { get; set; }
+        public bool AutoPropertiesOnly { get; set; }
+        public bool DebuggerOutput { get; set; }
+    }
+}
