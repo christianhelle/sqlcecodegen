01718288adfcd34cb2be6185716a98e708d877c4
christianhelle <christianhelle@localhost>
2013-10-07 12:24:50 +0200

    Introduce DAL options dependency

--- SQLCE Code Generator/CodeGenCore/DataAccessLayerGenerator.cs
+++ SQLCE Code Generator/CodeGenCore/DataAccessLayerGenerator.cs
@@ -19,6 +19,8 @@ namespace ChristianHelle.DatabaseTools.SqlCe.CodeGenCore
             return Code.ToString();
         }
 
+        public DataAccessLayerGeneratorOptions Options { get; set; }
+
         public virtual void GenerateCreateEntity() { }
         public abstract void GenerateSelectAll();
         public abstract void GenerateSelectBy();
